ed3fe907b5af2eea7b1b45257d3b46b17e6901f1
Ítalo Silva <91927240+devlibra-py@users.noreply.github.com>
2024-11-12 15:38:29 +0000

    alteracao da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Hello world!</h1>
+    return <h1>Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito, para que todo o que nele crê não pereça, mas tenha a vida eterna.</h1>
 }
 
 export default Home;

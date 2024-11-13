7ef3068bedfa56ca7eea7d54e1b7e05e173f9612
Ítalo Silva <91927240+devlibra-py@users.noreply.github.com>
2024-11-13 12:15:39 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito, para que todo o que nele crê não pereça, mas tenha a vida eterna.</h1>
+function Home() {
+  return (
+    <h1>
+      Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito,
+      para que todo o que nele crê não pereça, mas tenha a vida eterna.
+    </h1>
+  );
 }
 
 export default Home;
